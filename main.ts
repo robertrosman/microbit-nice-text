51bfebbd8d024b3a4b43c85807ef60cc4f4acfc8
Robert Rosman <robert.rosman@zendr.se>
2025-05-11 21:46:08 +0200

    Full alphabeth and working animation

--- main.ts
+++ main.ts
@@ -38,35 +38,61 @@ namespace NiceText {
 
     // Minecraft Alpha created by SinMedia Studios: https://fontstruct.com/fontstructions/show/2482281/minecraft-alpha
     const MINECRAFT_ALPHA = `
-      Aa  Bb  Cc  Dd. Ee  Ff  Gg  Hh. Ii Jj  Kk
-      ### ### ### ##. ### ### ### #.# #  ..# #.#
-      #.# #.# #.. #.# #.. #.. #.. #.# #  ..# #.#
-      ### ##. #.. #.# ##. ##. #.# ### #  ..# ##.
-      #.# #.# #.. #.# #.. #.. #.# #.# #  #.# #.#
-      #.# ### ### ##. ### #.. ### #.# #  ### #.#
+      Aa  Bb  Cc  Dd  Ee  Ff  Gg  Hh  Ii Jj  Kk  Ll  Mm    Nn    Oo  Pp  Qq   Rr  Ss  Tt  Uu  Vv  Ww    Xx  Yy  Zz  Åå  Ää  Öö  0   1  2   3   4   5   6   7   8   9   . ! ?   +   -   *   /   : =
+      ### ### ### ##. ### ### ### #.# #  ..# #.# #.. #...# #...# ### ### ###. ### ### ### #.# #.# #...# #.# #.# ### .#. #.# #.# ### .# ### ### #.# ### ### ### ### ### . # ### ... ... ... ..# . ...
+      #.# #.# #.. #.# #.. #.. #.. #.# #  ..# #.# #.. ##.## ##..# #.# #.# #.#. #.# #.. .#. #.# #.# #...# #.# #.# ..# ... ... ... #.# ## ..# ..# #.# #.. #.. ..# #.# #.# . # ..# .#. ... #.# ..# . ###
+      ### ##. #.. #.# ##. ##. #.# ### #  ..# ##. #.. #.#.# #.#.# #.# ### #.#. ##. ### .#. #.# #.# #.#.# .#. ### .#. ### ### ### #.# .# ### .## ### ### ### .#. .#. ### . # .#. ### ### .#. .#. # ...
+      #.# #.# #.. #.# #.. #.. #.# #.# #  #.# #.# #.. #...# #..## #.# #.. #.#. #.# ..# .#. #.# #.# #.#.# #.# .#. #.. ### ### #.# #.# .# #.. ..# ..# ..# #.# #.. #.# ..# . . ... .#. ... #.# #.. . ###
+      #.# ### ### ##. ### #.. ### #.# #  ### #.# ### #...# #...# ### #.. #### #.# ### .#. ### .#. .#.#. #.# .#. ### #.# #.# ### ### .# ### ### ..# ### ### #.. ### ### # # .#. ... ... ... #.. # ...
     `
 
     /**
      * show a scroll string
      * @param s      , eg: "Hello"
-     * @param dir    , eg: NICE_TEXT_DIR.LEFT
-     * @param rotate , eg: NICE_TEXT_ROTATE.SR_0
      * @param delay  , eg: 100
      */
-    //% blockId="NICE_TEXT_SHOWSTRING" block="scroll string %s|dir %dir|rotate %rotate|delay %delay"
+    //% blockId="NICE_TEXT_SHOWSTRING" block="scroll string %s|delay %delay"
     //% weight=100 blockGap=8
-    export function showString(s: string, delay: number, letterSpacing: number, wordSpacing: number): void {
-        let L = s.length + 1
+    export function showString(message: string, delay: number, letterSpacing: number, wordSpacing: number): void {
+        if (message == '') return
 
-        if (s == '') return
+        const fontRows = MINECRAFT_ALPHA
+            .split('\n')
+            .filter(row => row.trim().length > 0)
+            .map(row => row
+                .split(' ')
+                .filter(letter => letter.trim().length > 0)
+            )
+        
+        const imgRows = [
+            '.....',
+            '.....',
+            '.....',
+            '.....',
+            '.....'
+        ]
 
-        s = ' ' + s + ' '
-        let img: Image = null
+        message.split('').forEach(char => {
+            if (char === ' ') {
+                addSpacing(imgRows, wordSpacing - letterSpacing)
+                return
+            }
 
-        const fontRows = MINECRAFT_ALPHA.split('\n');
-        const imageRows = fontRows.join('\n').replace(' ', '.')
-        img = images.createImage('..#..') // TODO: must create image another way, dynamic strings not supported!
-        img.scrollImage(1,200);
+            const match = fontRows[0].find(letter => letter.includes(char))
+            if (!match) {
+                return
+            }
+
+            const index = fontRows[0].indexOf(match)
+            for (let y = 0; y < 5; y++) {
+                imgRows[y] += fontRows[y + 1][index]
+            }
+            addSpacing(imgRows, letterSpacing)
+        })
+
+        for (let offset = 0; offset <= imgRows[0].length; offset++) {
+            const img = showImage(imgRows, offset, delay);
+        }
     }
 
     /**
@@ -79,8 +105,33 @@ namespace NiceText {
     export function showNumber(n: number, delay: number, letterSpacing: number, wordSpacing: number): void {
         showString(n.toString(), delay, letterSpacing, wordSpacing)
     }
+
+    function showImage(imgRows: string[], offset: number, delay: number) {
+        let img = images.createImage(`
+            .....
+            .....
+            .....
+            .....
+            .....
+        `)
+
+        for (let x = offset; x < offset + 5; x++) {
+            for(let y = 0; y < 5; y++) {
+                img.setPixel(x - offset, y, imgRows[y].charAt(x) === '#')
+            }
+        }
+        img.showImage(0, delay);
+        return img
+    }
+
+    function addSpacing(imgRows: string[], spacing: number) {
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < spacing; x++) {
+                imgRows[y] += '.'
+            }
+        }
+    }
 }
 
 
-
-NiceText.showString('HELLO', 100, 1, 5);
+NiceText.showString('HELLO WORLD!', 300, 1, 5);
